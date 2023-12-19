28a70c5b38d28a1707f29bf0d34377e75cf214d1
Kacper Stolarczyk <01187591@pw.edu.pl>
2023-12-19 12:44:08 +0100

    Dodanie Eliminacji Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -4,11 +4,57 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+int eliminate(Matrix *mat, Matrix *b)
+{
+	int wiersze = mat->r;
+	int kolumny = mat->c + b->c;
 
-		return 0;
+	double macierz[wiersze][kolumny];
+
+	for(int w = 0; w < wiersze; w++)
+	{
+		for(int k = 0; k < kolumny; k++)
+		{
+			if(k + 1 == kolumny)
+			{
+				macierz[w][k] = b->data[w][0];
+			}
+			else macierz[w][k] = mat->data[w][k];
+		}
+	}
+
+	for(int i = 0; i < wiersze - 1; i++)
+	{
+		double A = macierz[i][i];
+		if(A == 0) return 1;
+
+		int x = i + 1;
+		while (x < wiersze)
+		{
+			double B = macierz[x][i];
+			double iloraz = B / A;
+
+			for(int k = 0; k < kolumny; k++)
+			{
+				macierz[x][k] -= macierz[i][k] * iloraz;
+			}
+
+			x++;
+		}
+	}
+
+	for(int w = 0; w < wiersze; w++)
+	{
+		for(int k = 0; k < kolumny; k++)
+		{
+			if(k + 1 == kolumny)
+			{
+				b->data[w][1] = macierz[w][k];
+			}
+			else mat->data[w][k] = macierz[w][k];
+		}
+	}
+
+	return 0;
 }
 
