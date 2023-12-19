4e64950824e2903735c04d075626b51633ea310e
Michał Marciniak <michal.mar725@gmail.com>
2023-12-19 15:41:58 +0100

    Dodanie wstecznego podstawienia

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,22 +1,35 @@
 #include "backsubst.h"
+
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+	//BLEDNE ROZMIARY MACIERZY
+	if (mat->r != mat->c || mat->r != b->r)
+		return 2;
 
-				return 0;
+	
+	for (int i = x->r-1; i >= 0; i--) 
+	{
+		if (mat->data[i][i] == 0) 
+		{
+			return 1; //Element diagonalny jest 0
+		}
+
+		x->data[i][0] = b->data[i][0] / mat->data[i][i];
+
+		for (int j = i-1; j >= 0; j--) 
+		{
+			b->data[j][0] -= mat->data[j][i] * x->data[i][0];
+		}
+
+	}
+	return 0;
+
 }
 
 
