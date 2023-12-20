8090e411d46b0b36fd8604a38ff32d31117132c2
Michał Marciniak <michal.mar725@gmail.com>
2023-12-20 18:45:28 +0100

    Dodanie wyboru elementu głównego   +plik A1 w danych

--- src/gauss.c
+++ src/gauss.c
@@ -4,13 +4,19 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
 int eliminate(Matrix *mat, Matrix *b)
 {
 	int wiersze = mat->r;
 	int kolumny = mat->c + b->c;
 
-	double macierz[wiersze][kolumny];
+	//double macierz[wiersze][kolumny]
+	double **macierz = (double**)malloc(wiersze * sizeof(double*));
+	for (int i = 0; i < wiersze; i++) {
+		macierz[i] = (double*)malloc(kolumny * sizeof(double));
+	}
 
+	//tworzy rozszerzona macierz
 	for(int w = 0; w < wiersze; w++)
 	{
 		for(int k = 0; k < kolumny; k++)
@@ -22,9 +28,11 @@ int eliminate(Matrix *mat, Matrix *b)
 			else macierz[w][k] = mat->data[w][k];
 		}
 	}
-
+	//eliminacja
 	for(int i = 0; i < wiersze - 1; i++)
 	{
+		p_select(macierz, wiersze, kolumny, i);
+
 		double A = macierz[i][i];
 		if(A == 0) return 1;
 
@@ -42,7 +50,7 @@ int eliminate(Matrix *mat, Matrix *b)
 			x++;
 		}
 	}
-
+	//zamienia oryginalna macierz
 	for(int w = 0; w < wiersze; w++)
 	{
 		for(int k = 0; k < kolumny; k++)
@@ -54,7 +62,34 @@ int eliminate(Matrix *mat, Matrix *b)
 			else mat->data[w][k] = macierz[w][k];
 		}
 	}
+	for (int i = 0; i < wiersze; i++)
+		free(macierz[i]);
+	free(macierz);
 
 	return 0;
 }
 
+void p_select(double **macierz, int rows, int cols, int diag) {
+	//wybiera największy element w kolumnie i zamienia miejscami
+
+	int max_index = diag;
+	double max_val = fabs(macierz[diag][diag]);
+
+	for (int i = diag; i <= rows - 1; i++) {
+		if (max_val < fabs(macierz[i][diag])) {
+			max_index = i;
+			max_val = fabs(macierz[i][diag]);
+		}
+	}
+
+	if (max_index == diag) 
+		return;
+
+	double tmp_val;
+	for( int i = 0; i < cols; i++) {
+
+		tmp_val = macierz[diag][i];
+		macierz[diag][i] = macierz[max_index][i];
+		macierz[max_index][i] = tmp_val;
+	}
+}
--- src/gauss.h
+++ src/gauss.h
@@ -2,11 +2,14 @@
 #define _GAUSS_H
 
 #include "mat_io.h"
+#include <stdlib.h>
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b);
+void p_select(double**, int, int, int);
 
 #endif
